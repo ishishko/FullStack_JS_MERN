aa2cbd6495e3a0761cce146f9080b52e092b956b
Ishishko <shishkoignacio@gmail.com>
2023-08-29 15:10:49 -0300

    FullStack JS | Backend Pacientes SCRUD Finalizado / Instalando react y dependencias Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  <BrowserRouter>
+    return <h1 className="font-bold text-2xl">Hola Vite</h1>;
+  </BrowserRouter>;
+}
+
+export default App;
