fac3af3a53c045f18bd7cb26127dc0d2ffd2705c
Ishishko <shishkoignacio@gmail.com>
2023-09-14 13:35:47 -0300

    FullStack JS | Corregido carga de pacientes

--- frontend/src/context/PacienteProvider.jsx
+++ frontend/src/context/PacienteProvider.jsx
@@ -2,6 +2,7 @@
 import { createContext, useState, useEffect } from "react";
 
 import clienteAxios from "../config/axios";
+import useAuth from "../hooks/useAuth";
 
 const PacientesContext = createContext();
 
@@ -9,6 +10,7 @@ const PacientesContext = createContext();
 const PacientesProvider = ({ children }) => {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+  const { auth } = useAuth();
 
   useEffect(() => {
     const obtenerPacientes = async () => {
@@ -29,7 +31,7 @@ const PacientesProvider = ({ children }) => {
       }
     };
     obtenerPacientes();
-  }, []);
+  }, [auth]);
 
   const guardarPaciente = async (paciente) => {
     const token = localStorage.getItem("token");
